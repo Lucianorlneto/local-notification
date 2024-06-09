429cf875f63cbb3b88c7cf5e01cef8a9bbeb1d09
Luciano Lucio Neto <lucianorlneto@more.tt>
2024-06-09 20:50:08 -0300

    feat: adds home to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "../Button";
 import NotificationsDrawer from "../NotificationsDrawer";
 import { IoIosNotifications } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,8 +10,14 @@ const Header: React.FC = () => {
   const toggleDrawer = () => setIsOpen((value) => !value);
 
   return (
-    <div className="p-8 flex bg-gray-900 h-20  items-center justify-start">
+    <div className="p-8 flex bg-gray-900 h-20  items-center justify-between">
+      <div>
+        <Link className="text-gray-200 text-2xl font-semibold" to={"/"}>
+          Home
+        </Link>
+      </div>
       <Button
+        className=""
         data-cy="notifications-button"
         onClick={toggleDrawer}
         icon={<IoIosNotifications />}
