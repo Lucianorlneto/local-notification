4534225f5be9671f61a0c6d4f75f5a46b9fbe985
Luciano Lucio Neto <lucianorlneto@more.tt>
2024-06-09 22:48:10 -0300

    feat: adds notification data for testing

--- src/constants.ts
+++ src/constants.ts
@@ -1,46 +1,162 @@
 export const testNotifications: CustomNotification[] = [
   {
-    description:
-      " lalalala lalalala lalalalalalalalalalalala lalalala lalalala lalalala lalalala lalalala lalalalalalalalalalalalalalalala lalalala lalalala lalalala lalalala lalalalalalalalalalalala lalalala lalalala lalalala",
     recordId: 1,
-    title: "Test 1",
-    type: "NORMAL",
-    isRead: false,
-  },
-  {
-    description: "bobobo bo bo bo bo bo bo bo bo bo bo",
-    recordId: 2,
-    title: "Test 2",
+    title: "Soup - Campbells, Lentil",
+    description:
+      "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.",
     type: "SEVERE",
     isRead: false,
   },
   {
-    description: "oIfd",
-    recordId: 3,
-    title: "Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3",
-    type: "WARNING",
-    isRead: false,
-  },
-  {
-    description:
-      " lalalala lalalala lalalalalalalalalalalala lalalala lalalala lalalala lalalala lalalala lalalalalalalalalalalalalalalala lalalala lalalala lalalala lalalala lalalalalalalalalalalala lalalala lalalala lalalala",
-    recordId: 1,
-    title: "Test 1",
-    type: "NORMAL",
-    isRead: false,
-  },
-  {
-    description: "bobobo bo bo bo bo bo bo bo bo bo bo",
     recordId: 2,
-    title: "Test 2",
+    title: "Foil Cont Round",
+    description:
+      "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
     type: "SEVERE",
     isRead: false,
   },
   {
-    description: "oIfd",
     recordId: 3,
-    title: "Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3",
+    title: "Doilies - 8, Paper",
+    description:
+      "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+    type: "NORMAL",
+    isRead: false,
+  },
+  {
+    recordId: 4,
+    title: "Beer - True North Strong Ale",
+    description:
+      "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 5,
+    title: "Ham Black Forest",
+    description:
+      "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 6,
+    title: "Sauce - Marinara",
+    description:
+      "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 7,
+    title: "Eggroll",
+    description:
+      "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 8,
+    title: "Nantucket - 518ml",
+    description:
+      "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    type: "NORMAL",
+    isRead: false,
+  },
+  {
+    recordId: 9,
+    title: "Coffee - Flavoured",
+    description:
+      "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+    type: "NORMAL",
+    isRead: false,
+  },
+  {
+    recordId: 10,
+    title: "Canada Dry",
+    description:
+      "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 11,
+    title: "Pineapple - Regular",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 12,
+    title: "Bread - Hot Dog Buns",
+    description:
+      "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+    type: "NORMAL",
+    isRead: false,
+  },
+  {
+    recordId: 13,
+    title: "Carbonated Water - Raspberry",
+    description:
+      "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 14,
+    title: "Beer - True North Strong Ale",
+    description:
+      "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
     type: "WARNING",
+    isRead: false,
+  },
+  {
+    recordId: 15,
+    title: "Coffee - 10oz Cup 92961",
+    description:
+      "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 16,
+    title: "Wine - White, Ej",
+    description:
+      "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    type: "SEVERE",
+    isRead: false,
+  },
+  {
+    recordId: 17,
+    title: "Wine - White, Colubia Cresh",
+    description:
+      "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    type: "WARNING",
+    isRead: false,
+  },
+  {
+    recordId: 18,
+    title: "Crush - Orange, 355ml",
+    description:
+      "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    type: "NORMAL",
+    isRead: false,
+  },
+  {
+    recordId: 19,
+    title: "Cheese - Romano, Grated",
+    description:
+      "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+    type: "WARNING",
+    isRead: false,
+  },
+  {
+    recordId: 20,
+    title: "Salt - Celery",
+    description:
+      "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
+    type: "NORMAL",
     isRead: false,
   },
 ];
